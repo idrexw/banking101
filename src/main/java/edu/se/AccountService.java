63e676e559062248fbcfc10298d60efb478d8e8d
luifuata <lfuat1@brockport.edu>
2026-04-07 16:27:05 -0400

    The beginning of AccountService.java

---
+++ src/main/java/edu/se/AccountService.java
@@ -0,0 +1,11 @@
+package edu.se;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountService {
+    private List<Account> accounts = new ArrayList<>();
+
+    public Account createAccount(String type, String name, double initialBalance) {
+    }
+
