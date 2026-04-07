46dcfc1de7a4ca4a5aa62d9d91bfc9a68e87056f
Andrew Maher <andrewmaher845@gmail.com>
2026-04-07 16:25:35 -0400

    Account Class

---
+++ src/main/java/edu/se/Account.java
@@ -0,0 +1,20 @@
+package edu.se;
+
+public class Account {
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+    public String accountType;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
