e9a95df381262ad374e796a01966cef074554f42
Andrew Maher <andrewmaher845@gmail.com>
2026-04-08 12:58:22 -0400

    Working createAccount()

--- src/main/java/edu/se/Account.java
+++ src/main/java/edu/se/Account.java
@@ -1,19 +1,19 @@
 package edu.se;
 
 public class Account {
-    private int accountNumber;
+    private long accountNumber;
     private String accountHolder;
     private double balance;
     public String accountType;
 
-    public Account(int id, String s, double bal, String s2) {
+    public Account(long id, String s, double bal, String s2) {
         this.accountNumber = id;
         this.accountHolder = s;
         this.balance = bal;
         this.accountType = s2;
     };
 
-    public int getAccountNumber() {
+    public long getAccountNumber() {
         return accountNumber;
     }
 
@@ -39,6 +39,6 @@ public class Account {
 
     @Override
     public String toString() {
-        return "Account Num:" + this.accountNumber + ", Name: " + this.accountHolder + ", Current Balance: " + this.balance + ", Type: " + this.accountType;
+        return "Account Num: " + this.accountNumber + "\nName: " + this.accountHolder + "\nCurrent Balance: " + this.balance + "\nType: " + this.accountType;
     }
 }
--- src/main/java/edu/se/Main.java
+++ src/main/java/edu/se/Main.java
@@ -1,9 +1,9 @@
 package edu.se;
 
 public class Main {
-    public static void main() {
-        AccountService Control = new accountService();
-        Control.createAccount("Checking", "Andrew Maher", 100.00);
-        System.out.println(Control.accounts.get(0).toString());
+    public static void main(String[] args) {
+        AccountService Control = new AccountService();
+        Account newAccount = Control.createAccount("Checking", "Andrew Maher", 100.00);
+        System.out.println(newAccount);
     }
 }
